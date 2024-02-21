29aa685c76b228ffeed58f151ccd549522509896
dbakin <dbakin@ptsecurity.com>
2024-02-21 22:42:20 +0300

    Add some tests

--- src/test/kotlin/iLangTests.kt
+++ src/test/kotlin/iLangTests.kt
@@ -6,28 +6,8 @@ import org.junit.jupiter.api.Test
 @Suppress("ClassName")
 class iLangTests {
     @Test
-    fun array_count_test() {
-        ParserTestsRunner.run("array_count")
-    }
-    
-    @Test
-    fun array_min_max_test() {
-        ParserTestsRunner.run("array_min_max")
-    }
-    
-    @Test
-    fun fib_test() {
-        ParserTestsRunner.run("fib")
-    }
-    
-    @Test
-    fun gcd_test() {
-        ParserTestsRunner.run("gcd")
-    }
-    
-    @Test
-    fun graph_test() {
-        ParserTestsRunner.run("graph")
+    fun array_avg_test() {
+        ParserTestsRunner.run("array_avg")
     }
     
     @Test
@@ -36,13 +16,23 @@ class iLangTests {
     }
     
     @Test
-    fun fact_test() {
-        ParserTestsRunner.run("fact")
+    fun array_linear_search_test() {
+        ParserTestsRunner.run("array_linear_search")
     }
     
     @Test
-    fun array_linear_search_test() {
-        ParserTestsRunner.run("array_linear_search")
+    fun array_count_test() {
+        ParserTestsRunner.run("array_count")
+    }
+    
+    @Test
+    fun bubble_sort_test() {
+        ParserTestsRunner.run("bubble_sort")
+    }
+    
+    @Test
+    fun linked_list_test() {
+        ParserTestsRunner.run("linked_list")
     }
     
     @Test
@@ -51,8 +41,43 @@ class iLangTests {
     }
     
     @Test
-    fun array_avg_test() {
-        ParserTestsRunner.run("array_avg")
+    fun quick_sort_test() {
+        ParserTestsRunner.run("quick_sort")
+    }
+    
+    @Test
+    fun graph_test() {
+        ParserTestsRunner.run("graph")
+    }
+    
+    @Test
+    fun fib_test() {
+        ParserTestsRunner.run("fib")
+    }
+    
+    @Test
+    fun pow_test() {
+        ParserTestsRunner.run("pow")
+    }
+    
+    @Test
+    fun is_prime_test() {
+        ParserTestsRunner.run("is_prime")
+    }
+    
+    @Test
+    fun gcd_test() {
+        ParserTestsRunner.run("gcd")
+    }
+    
+    @Test
+    fun fact_test() {
+        ParserTestsRunner.run("fact")
+    }
+    
+    @Test
+    fun array_min_max_test() {
+        ParserTestsRunner.run("array_min_max")
     }
     
     
