4e5558812a6f65a46752468d052fa1c8ed3ba37e
Evgeny Bukreev <ebukreev@ptsecurity.com>
2024-03-06 16:45:44 +0300

    implement CompileTimeEvaluator

--- src/main/kotlin/edu/itmo/ilang/CompileTimeEvaluator.kt
+++ src/main/kotlin/edu/itmo/ilang/CompileTimeEvaluator.kt
@@ -1,9 +1,21 @@
 package edu.itmo.ilang
 
-import edu.itmo.ilang.ir.Expression
+import edu.itmo.ilang.ir.*
 
 object CompileTimeEvaluator {
     fun evaluateInt(expression: Expression): Int {
-        TODO()
+        return when (expression) {
+            is IntegralLiteral -> expression.value
+
+            is PlusExpression -> evaluateInt(expression.left) + evaluateInt(expression.right)
+
+            is MulExpression -> evaluateInt(expression.left) * evaluateInt(expression.right)
+
+            is DivExpression -> evaluateInt(expression.left) / evaluateInt(expression.right)
+
+            is ModExpression -> evaluateInt(expression.left) % evaluateInt(expression.right)
+
+            else -> TODO()
+        }
     }
 }
