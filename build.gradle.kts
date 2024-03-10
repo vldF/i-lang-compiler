56d277cd155f6dd81f5be4010eaa800ead8731ff
vldf <vladislavf7@gmail.com>
2024-03-10 15:37:44 +0300

    Remove useless comments from gradle config

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,27 +16,20 @@ ext {
 
 
 repositories {
-    // Use Maven Central for resolving dependencies.
     mavenCentral()
 }
 
 dependencies {
     antlr("org.antlr:antlr4:4.5")
 
-    // Use the Kotlin JUnit 5 integration.
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-
-    // Use the JUnit 5 integration.
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.3")
-
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    // This dependency is used by the application.
     implementation("com.google.guava:guava:32.1.1-jre")
     implementation("org.bytedeco:llvm-platform:17.0.6-1.5.10")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.3")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 }
 
-// Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(20))
@@ -44,12 +37,10 @@ java {
 }
 
 application {
-    // Define the main class for the application.
     mainClass.set("edu.itmo.ilang.AppKt")
 }
 
 tasks.named<Test>("test") {
-    // Use JUnit Platform for unit tests.
     useJUnitPlatform()
 }
 
