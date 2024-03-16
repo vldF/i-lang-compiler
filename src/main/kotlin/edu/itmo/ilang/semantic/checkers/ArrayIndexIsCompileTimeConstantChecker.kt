830b14f4c5cda333ee2eb91c9730d4a6f34a5415
vldf <vladislavf7@gmail.com>
2024-03-16 21:56:01 +0300

    Add ArrayIndexIsCompileTimeConstantChecker template

---
+++ src/main/kotlin/edu/itmo/ilang/semantic/checkers/ArrayIndexIsCompileTimeConstantChecker.kt
@@ -0,0 +1,9 @@
+package edu.itmo.ilang.semantic.checkers
+
+import edu.itmo.ilang.ir.Program
+
+class ArrayIndexIsCompileTimeConstantChecker : Checker {
+    override fun check(program: Program) {
+        TODO("Not yet implemented")
+    }
+}
