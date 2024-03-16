0185da2939527ea7e7ba3b76bff61531ec1a54ee
Evgeny Bukreev <ebukreev@ptsecurity.com>
2024-03-16 10:40:15 +0300

    implement DeadCodeEliminator

--- src/main/kotlin/edu/itmo/ilang/semantic/transformations/DeadCodeEliminator.kt
+++ src/main/kotlin/edu/itmo/ilang/semantic/transformations/DeadCodeEliminator.kt
@@ -1,9 +1,33 @@
 package edu.itmo.ilang.semantic.transformations
 
-import edu.itmo.ilang.ir.Program
+import edu.itmo.ilang.ir.*
 
 class DeadCodeEliminator : Transformer {
     override fun transform(program: Program) {
-        TODO("Not yet implemented")
+        removeDeadCode(program)
+    }
+
+    private fun removeDeadCode(irEntry: IrEntry) {
+        when(irEntry) {
+            is Program -> irEntry.declarations.forEach { removeDeadCode(it) }
+            is Body -> {
+                val indexOfTerminatingStatement = irEntry.statements
+                    .indexOfFirst { it is Return || it is Break || it is Continue }
+
+                if (indexOfTerminatingStatement != -1) {
+                    irEntry.statements = irEntry.statements.take(indexOfTerminatingStatement + 1)
+                }
+                irEntry.statements.forEach { removeDeadCode(it) }
+            }
+            is ForLoop -> removeDeadCode(irEntry.body)
+            is IfStatement -> {
+                removeDeadCode(irEntry.thenBody)
+                irEntry.elseBody?.let { removeDeadCode(it) }
+            }
+            is WhileLoop -> removeDeadCode(irEntry.body)
+            is RoutineDeclaration -> irEntry.body?.let { removeDeadCode(it) }
+
+            else -> {}
+        }
     }
 }
---
+++ src/test/kotlin/TestDeadCodeEliminator.kt
@@ -0,0 +1,55 @@
+import edu.itmo.ilang.ir.*
+import edu.itmo.ilang.semantic.transformations.DeadCodeEliminator
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+class TestDeadCodeEliminator {
+    @Test
+    fun simpleTest() {
+        val example = """
+            routine main() : integer is 
+                return 5
+                var a is 6
+                var b is 3
+            end
+        """.trimIndent()
+
+        val ir = generateIr(example)
+
+        assertTrue { (ir.declarations.first() as RoutineDeclaration).body!!.statements.last() is VariableDeclaration }
+
+        DeadCodeEliminator().transform(ir)
+
+        assertTrue { (ir.declarations.first() as RoutineDeclaration).body!!.statements.last() is Return }
+    }
+
+    @Test
+    fun testLoop() {
+        val example = """
+            routine isSorted(arr : array[] integer) : bool is
+                for i in 2..(arr.size) loop
+                    var b is 3 
+                    break
+                    var a is 5
+                    return false
+                end
+            
+                return true
+            end
+        """.trimIndent()
+
+        val ir = generateIr(example)
+
+        assertTrue {
+            ((ir.declarations.first() as RoutineDeclaration).body!!.statements.first() as ForLoop)
+                .body.statements.last() is Return
+        }
+
+        DeadCodeEliminator().transform(ir)
+
+        assertTrue {
+            ((ir.declarations.first() as RoutineDeclaration).body!!.statements.first() as ForLoop)
+                .body.statements.last() is Break
+        }
+    }
+}
