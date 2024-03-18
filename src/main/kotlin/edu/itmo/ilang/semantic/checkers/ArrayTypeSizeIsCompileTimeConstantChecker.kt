c0377ecd95b582a61167bf4f09acdf915cc5eb6e
vldf <vladislavf7@gmail.com>
2024-03-18 12:01:13 +0300

    Delete ArrayTypeSizeIsCompileTimeConstantChecker

--- src/main/kotlin/edu/itmo/ilang/semantic/checkers/ArrayTypeSizeIsCompileTimeConstantChecker.kt
+++
@@ -1,9 +0,0 @@
-package edu.itmo.ilang.semantic.checkers
-
-import edu.itmo.ilang.ir.Program
-
-class ArrayTypeSizeIsCompileTimeConstantChecker : Checker {
-    override fun check(program: Program) {
-        TODO("Not yet implemented")
-    }
-}
